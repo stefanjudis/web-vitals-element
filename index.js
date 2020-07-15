abef3ddfec77eb26f7769cbd7b173eb5a29a1f7a
stefan judis <stefanjudis@gmail.com>
2020-07-15 08:18:05 +0200

    Add improvements

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 import * as webVitals from 'web-vitals';
 
+const MS_UNIT = 'ms';
+
 // borrowed from the vitals extension
 // https://github.com/GoogleChrome/web-vitals-extension/blob/master/src/browser_action/vitals.js#L20-L23
 const METRIC_CONFIG = new Map([
-  ['LCP', { threshold: 2500, explainerURL: 'https://web.dev/lcp/' }],
-  ['FID', { threshold: 100, explainerURL: 'https://web.dev/fid/' }],
+  [
+    'LCP',
+    { threshold: 2500, explainerURL: 'https://web.dev/lcp/', unit: MS_UNIT },
+  ],
+  [
+    'FID',
+    { threshold: 100, explainerURL: 'https://web.dev/fid/', unit: MS_UNIT },
+  ],
   ['CLS', { threshold: 0.1, explainerURL: 'https://web.dev/cls/' }],
   // todo check the thresholds for the following
-  ['FCP', { threshold: 2500, explainerURL: 'https://web.dev/fcp/' }],
+  [
+    'FCP',
+    { threshold: 2500, explainerURL: 'https://web.dev/fcp/', unit: MS_UNIT },
+  ],
   [
     'TTFB',
-    { threshold: 2500, explainerURL: 'https://web.dev/time-to-first-byte/' },
+    {
+      threshold: 2500,
+      explainerURL: 'https://web.dev/time-to-first-byte/',
+      unit: MS_UNIT,
+    },
   ],
 ]);
 
@@ -52,7 +67,7 @@ class WebVitals extends HTMLElement {
       <dl>
         ${[...this.metrics]
           .map(([key, metric]) => {
-            const { explainerURL, isFinal, threshold, value } = metric;
+            const { explainerURL, isFinal, threshold, unit, value } = metric;
             let classes = '';
 
             if (isFinal) {
@@ -63,7 +78,9 @@ class WebVitals extends HTMLElement {
             return `
             <div class="${classes}">
               <dt><a href="${explainerURL}">${key}</a></dt>
-              <dd>${value ? `${Math.floor(value)}ms` : '...'}</dd>
+              <dd>${
+                value ? `${Math.floor(value)}${unit ? unit : ''}` : '...'
+              }</dd>
             </div>
           `;
           })
